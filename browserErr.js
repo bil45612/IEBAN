3c631e843fa70dfca2a729f49bb8b2608393584d
bil45612 <bil45612@gmail.com>
2020-07-14 14:28:54 +0800

    Update browserErr.js

--- browserErr.js
+++ browserErr.js
@@ -5,7 +5,7 @@ videojs.registerPlugin('browserErr', function() {
         "errors": {
           "BROWSER_ERR": {
             "headline": "Your Browser is not supported",
-            "message": "IE is not a supported browser, please change the browser that you are using."
+            "message": "IE 不支援, please change the browser that you are using."
           }
          }
   });
