d7b1b430843eff017b0ec634ec8ed4925c3114b3
bil45612 <bil45612@gmail.com>
2020-07-14 10:12:36 +0800

    Add files via upload

---
+++ IEban.js
@@ -0,0 +1,17 @@
+videojs.registerPlugin('browserErr', function() {
+  var myPlayer = this;
+
+  myPlayer.errors({
+        "errors": {
+          "BROWSER_ERR": {
+            "headline": "Your Browser is not supported",
+            "message": "IE is not a supported browser, please change the browser that you are using."
+          }
+        }
+  });
+  myPlayer.on("loadstart", function () {
+     if (videojs.Hls && window.navigator.userAgent.indexOf("Trident") > 0) {
+      myPlayer.error({code:'BROWSER_ERR'});
+     }
+  });
+});
