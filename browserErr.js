678daad78ea717fa44b4c989c3bde2742f376a0c
bil45612 <bil45612@gmail.com>
2020-07-14 14:28:04 +0800

    Update browserErr.js

--- browserErr.js
+++ browserErr.js
@@ -8,11 +8,10 @@ videojs.registerPlugin('browserErr', function() {
             "message": "IE is not a supported browser, please change the browser that you are using."
           }
          }
-    dismiss: false
   });
   myPlayer.on("loadstart", function () {
      if (videojs.Hls && window.navigator.userAgent.indexOf("Trident") > 0) {
-      myPlayer.error({code:'BROWSER_ERR'});
+      myPlayer.error({code:'BROWSER_ERR',dismiss: false});
      }
   });
 });
